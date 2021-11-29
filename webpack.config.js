9d0441e88e5d9edbe9fc00f5001b86e9840507a4
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-11-29 11:43:40 +0100

    Add package.json, necessary modules and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: './js/main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|webp|git|)$/,
+        use: [
+          {
+            loader: 'img-optimize-loader',
+            options: {
+              name: 'assets/imgs/[contenthash].[ext]',
+              compress: {
+                webp: true,
+                disableOnDevelopment: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: './css/[name].css' })],
+  mode: 'development',
+  devtool: 'inline-source-map',
+}
